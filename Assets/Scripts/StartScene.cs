01bb7447b9badc411f292ad9bdcd61fe10ca79b3
eatdreamcat <meow.std@outlook.com>
2020-03-24 16:59:08 +0800

    fff

---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+
+    public Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
